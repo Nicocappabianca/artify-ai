6f1452948e01efe8c907a47d910eca74332e749b
nicocappabianca <nicolas.cappabianca@gmail.com>
2023-11-01 14:36:35 -0300

    chore: setup firebase

---
+++ utils/constants.ts
@@ -0,0 +1,4 @@
+export const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY ?? "";
+export const HUGGING_FACE_TOKEN = process.env.HUGGING_FACE_TOKEN ?? "";
+export const STABLE_DIFFUSION_API =
+  "https://api-inference.huggingface.co/models/CompVis/stable-diffusion-v1-4";
---
+++ utils/firebase/firebase-config.ts
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { FIREBASE_API_KEY } from "./constants";
+
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: "artify-f893b.firebaseapp.com",
+  projectId: "artify-f893b",
+  storageBucket: "artify-f893b.appspot.com",
+  messagingSenderId: "285770654532",
+  appId: "1:285770654532:web:6bfac869d8501af00e6d11",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const Auth = getAuth(app);
+const Provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+const POST_COLLECTION_NAME = "post";
+
+export { Auth, Provider, db, storage, POST_COLLECTION_NAME };
