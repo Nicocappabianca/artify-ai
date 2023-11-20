94741534a3c9906117d20bb18a973ae156d329ff
nicocappabianca <nicolas.cappabianca@gmail.com>
2023-11-20 16:45:20 -0300

    fix(authOptions): build error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
-import NextAuth from "next-auth/next";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
@@ -10,10 +10,5 @@ export const authOptions = {
   ],
 };
 
-export const handler = NextAuth(authOptions);
-
-export enum AuthProviders {
-  GOOGLE = "google",
-}
-
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
