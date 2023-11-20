588987166396b0fe9f36dd4e63851c5df0cb09a0
nicocappabianca <nicolas.cappabianca@gmail.com>
2023-11-20 16:29:23 -0300

    feat(Post): add prompt text preview

--- components/Post.tsx
+++ components/Post.tsx
@@ -6,9 +6,10 @@ type PostProps = {
   image: string;
   userImage: string;
   userName: string;
+  prompt: string;
 };
 
-const Post: FC<PostProps> = ({ image, userImage, userName }) => {
+const Post: FC<PostProps> = ({ image, userImage, userName, prompt }) => {
   return (
     <div className="relative w-72 h-72 sm:w-96 sm:h-96 mx-auto group cursor-pointer">
       <Image
@@ -18,7 +19,7 @@ const Post: FC<PostProps> = ({ image, userImage, userName }) => {
         className="rounded-lg bg-slate-400/25 animate-pulse"
         onLoadingComplete={(image) => image.classList.remove("bg-slate-400/25", "animate-pulse")}
       />
-      <div className="absolute inset-0 bg-black/60 rounded-lg hidden group-hover:flex items-start justify-start p-4">
+      <div className="absolute inset-0 bg-black/60 rounded-lg hidden group-hover:flex flex-col items-start justify-between p-4">
         <div className="flex items-center space-x-2">
           <div className="w-10 h-10 relative">
             <Image
@@ -30,6 +31,7 @@ const Post: FC<PostProps> = ({ image, userImage, userName }) => {
           </div>
           <p className="text-white font-semibold">{userName}</p>
         </div>
+        <p className="text-white truncate max-w-full">{prompt}</p>
       </div>
     </div>
   );
--- components/PostsList.tsx
+++ components/PostsList.tsx
@@ -10,9 +10,9 @@ const PostsList: FC<PostsListProps> = ({ posts }) => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-6 w-fit mx-auto">
       {posts.map((post) => {
-        const { id, image, userName, userImage } = post;
+        const { id, image, userName, userImage, prompt } = post;
         return image ? (
-          <Post key={id} image={image} userName={userName} userImage={userImage} />
+          <Post key={id} image={image} userName={userName} userImage={userImage} prompt={prompt} />
         ) : null;
       })}
     </div>
