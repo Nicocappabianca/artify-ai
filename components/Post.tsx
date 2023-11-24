136ad389cabacbaa43091c405457e037f44ea510
nicocappabianca <nicolas.cappabianca@gmail.com>
2023-11-24 11:57:39 -0300

    feat(Post): download button

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { FC } from "react";
+import { DownloadIcon } from "@/components/icons";
 import Image from "next/image";
 
 type PostProps = {
@@ -19,19 +20,24 @@ const Post: FC<PostProps> = ({ image, userImage, userName, prompt }) => {
         className="rounded-lg bg-slate-400/25 animate-pulse"
         onLoadingComplete={(image) => image.classList.remove("bg-slate-400/25", "animate-pulse")}
       />
-      <div className="absolute inset-0 bg-black/60 rounded-lg hidden group-hover:flex flex-col items-start justify-between p-4">
-        <div className="flex items-center space-x-2">
-          <div className="w-10 h-10 relative">
-            <Image
-              className="rounded-full"
-              src={userImage}
-              layout="fill"
-              alt={`${userName} avatar`}
-            />
+      <div className="absolute inset-0 bg-black/60 rounded-lg hidden [@media(hover:hover)]:group-hover:flex flex-col items-start justify-between p-4">
+        <div className="flex justify-between items-center w-full">
+          <div className="flex items-center">
+            <div className="w-10 h-10 relative">
+              <Image
+                className="rounded-full"
+                src={userImage}
+                layout="fill"
+                alt={`${userName} avatar`}
+              />
+            </div>
+            <p className="text-white font-semibold text-left ml-2 ">{userName}</p>
           </div>
-          <p className="text-white font-semibold text-left">{userName}</p>
+          <button className="rounded-full hover:bg-slate-400/30 p-2 transition">
+            <DownloadIcon className="text-white w-6 h-6" />
+          </button>
         </div>
-        <p className="text-white truncate max-w-full">{prompt}</p>
+        <p className="text-white text-sm truncate max-w-full">{prompt}</p>
       </div>
     </div>
   );
