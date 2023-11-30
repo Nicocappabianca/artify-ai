186b7c99189a7216b9433b68f767f59e2d0b905a
nicocappabianca <nicolas.cappabianca@gmail.com>
2023-11-30 16:08:00 -0300

    fix(Button): add twMerge

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,11 +1,12 @@
 import { ButtonHTMLAttributes, FC } from "react";
+import { twMerge } from "tailwind-merge";
 
 const buttonClassNames =
   "bg-slate-400 rounded-md text-slate-900 border border-slate-900 py-1 px-1.5 hover:bg-slate-900 hover:text-slate-400 hover:border hover:border-slate-400 transition active:scale-95 disabled:opacity-40 disabled:pointer-events-none";
 
 const Button: FC<ButtonHTMLAttributes<HTMLButtonElement>> = (props) => {
   return (
-    <button {...props} className={`${buttonClassNames} ${props.className || ""}`}>
+    <button {...props} className={twMerge(buttonClassNames, props.className)}>
       {props.children}
     </button>
   );
