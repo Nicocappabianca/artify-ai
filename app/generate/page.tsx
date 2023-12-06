d40bbb78ac51d5bab11a92035e3a7f6dec95b07f
nicocappabianca <nicolas.cappabianca@gmail.com>
2023-12-06 16:10:57 -0300

    chore(Generate Page): improve title

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -20,9 +20,7 @@ export default async function GeneratePage() {
 
   return (
     <section className="pt-8 sm:pt-12 text-center">
-      <h1 className="text-lg text-white sm:text-3xl font-bold">
-        🖌️ Prompt Your Creativity with Artify 🎨
-      </h1>
+      <h1 className="text-lg text-white sm:text-3xl font-bold">🖌️ Prompt Your Creativity 🎨</h1>
       <p className="pt-3 pb-8 sm:pb-12 text-md sm:text-lg max-w-xs sm:max-w-none mx-auto">
         Unleash Your Imagination and Create Art with AI-Powered Generation
       </p>
